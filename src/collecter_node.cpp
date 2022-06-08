8bfc83588dba8f23bbca2920052c7652c54c52e0
KonstFed <konstantin_fedorov_03@mail.ru>
2022-06-08 18:02:32 +0300

    collector_node.cpp process all turtle_cmd topics to turtles_cmd topic

--- src/collecter_node.cpp
+++ src/collecter_node.cpp
@@ -1,68 +1,83 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <homework1_konstantin_evsey/TwistArray.h>
+#include <turtlesim/Pose.h>
+#include <geometry_msgs/PoseArray.h>
+#include <geometry_msgs/Point.h>
+#include <geometry_msgs/Quaternion.h>
 #include <math.h>
 
 const char* SERVICE_KILL_TURTLE = "/kill";
 const char* SERVICE_SPAWN_TURTLE = "/spawn";
 
-homework1_konstantin_evsey::TwistArray mymsg;
+homework1_konstantin_evsey::TwistArray msg_cmd;
+geometry_msgs::PoseArray msg_poses;
+
 bool listened[] = {false,false,false,false};
 
 ros::Publisher pub_turtles_cmd;
 
 void send_turtles_cmd()
 {
-    pub_turtles_cmd.publish(mymsg);
-    ROS_INFO("send_turtles_cmd is called!!!!");
+    ROS_INFO("send_turtles_cmd is called.");
+    pub_turtles_cmd.publish(msg_cmd);
     listened[0] = false;
     listened[1] = false;
     listened[2] = false;
     listened[3] = false;
 }
 
-void callback_turtle1_cmd(geometry_msgs::Twist msg)
+template<int turtleNum>
+void callback_turtle_cmd(geometry_msgs::Twist msg)
 {
-    mymsg.turtle1.linear = msg.linear;
-    mymsg.turtle1.angular = msg.angular;
-    listened[0] = true;
-    if (listened[0] && listened[1] && listened[2] && listened[3])
+    switch(turtleNum)
     {
-        send_turtles_cmd();
+        case 1:
+            msg_cmd.turtle1.linear = msg.linear;
+            msg_cmd.turtle1.angular = msg.angular;
+            break;
+        case 2:
+            msg_cmd.turtle2.linear = msg.linear;
+            msg_cmd.turtle2.angular = msg.angular;
+            break;
+        case 3:
+            msg_cmd.turtle3.linear = msg.linear;
+            msg_cmd.turtle3.angular = msg.angular;
+            break;
+        case 4:
+            msg_cmd.turtle4.linear = msg.linear;
+            msg_cmd.turtle4.angular = msg.angular;
+            break;
     }
+    listened[turtleNum-1] = true;
 }
 
-void callback_turtle2_cmd(geometry_msgs::Twist msg)
+template<int turtleNum>
+void callback_turtles_pose(turtlesim::Pose msg)
 {
-    mymsg.turtle2.linear = msg.linear;
-    mymsg.turtle2.angular = msg.angular;
-    listened[1] = true;
-    if (listened[0] && listened[1] && listened[2] && listened[3])
-    {
-        send_turtles_cmd();
-    }
-}
+    // TODO: understand transition between turtlesim::Pose to geometry_msgs::Pose
+    
+    // switch(turtleNum)
+    //     {
+    //         case 1:
+    //             msg_poses.poses.position.x = msg.x;
+    //             msg_poses.poses.position.y = msg.y;
+                
 
-void callback_turtle3_cmd(geometry_msgs::Twist msg)
-{
-    mymsg.turtle3.linear = msg.linear;
-    mymsg.turtle3.angular = msg.angular;
-    listened[2] = true;
-    if (listened[0] && listened[1] && listened[2] && listened[3])
-    {
-        send_turtles_cmd();
-    }
-}
-
-void callback_turtle4_cmd(geometry_msgs::Twist msg)
-{
-    mymsg.turtle4.linear = msg.linear;
-    mymsg.turtle4.angular = msg.angular;
-    listened[3] = true;
-    if (listened[0] && listened[1] && listened[2] && listened[3])
-    {
-        send_turtles_cmd();
-    }
+    //             break;
+    //         case 2:
+    //             msg_cmd.turtle2.linear = msg.linear;
+    //             msg_cmd.turtle2.angular = msg.angular;
+    //             break;
+    //         case 3:
+    //             msg_cmd.turtle3.linear = msg.linear;
+    //             msg_cmd.turtle3.angular = msg.angular;
+    //             break;
+    //         case 4:
+    //             msg_cmd.turtle4.linear = msg.linear;
+    //             msg_cmd.turtle4.angular = msg.angular;
+    //             break;
+    //     }
 }
 
 int main(int argc, char **argv)
@@ -71,12 +86,27 @@ int main(int argc, char **argv)
     ros::init(argc, argv,"turtles_spawner");
     ros::NodeHandle n;
     
-    ros::Subscriber sub_turtle1_cmd = n.subscribe("/turtle1_cmd",10,callback_turtle1_cmd);
-    ros::Subscriber sub_turtle2_cmd = n.subscribe("/turtle1_cmd",10,callback_turtle2_cmd);
-    ros::Subscriber sub_turtle3_cmd = n.subscribe("/turtle1_cmd",10,callback_turtle3_cmd);
-    ros::Subscriber sub_turtle4_cmd = n.subscribe("/turtle1_cmd",10,callback_turtle4_cmd);
+    ros::Subscriber sub_turtle1_cmd = n.subscribe("/turtle1_cmd",10,callback_turtle_cmd<1>);
+    ros::Subscriber sub_turtle2_cmd = n.subscribe("/turtle1_cmd",10,callback_turtle_cmd<2>);
+    ros::Subscriber sub_turtle3_cmd = n.subscribe("/turtle1_cmd",10,callback_turtle_cmd<3>);
+    ros::Subscriber sub_turtle4_cmd = n.subscribe("/turtle1_cmd",10,callback_turtle_cmd<4>);
 
-    // pub_turtles_cmd = n.advertise("/turtles_cmd",1,)
-    
-    ros::spin();
+    // ros::Subscriber sub_turtle1_pose = n.subscribe("/turtle2/pose",10,callback_turtle1_cmd);
+    // ros::Subscriber sub_turtle2_pose = n.subscribe("/turtle3/pose",10,callback_turtle2_cmd);
+    // ros::Subscriber sub_turtle3_pose = n.subscribe("/turtle4/pose",10,callback_turtle3_cmd);
+    // ros::Subscriber sub_turtle4_pose = n.subscribe("/turtle5/pose",10,callback_turtle4_cmd);
+
+    pub_turtles_cmd = n.advertise<homework1_konstantin_evsey::TwistArray>("/turtles_cmd",1);
+
+
+    ros::Rate rate(1);
+    while (ros::ok())
+    {
+        if (listened[0] && listened[1] && listened[2] && listened[3])
+        {
+            send_turtles_cmd();
+        }
+        ros::spinOnce();
+        rate.sleep();
+    }
 }
